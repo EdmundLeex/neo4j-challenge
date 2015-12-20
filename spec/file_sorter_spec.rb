0501e5a3eb4b46b7fd0c6d3601bc25fd60d972d4
Edmund Li <edmund.xz.lee@gmail.com>
2015-12-20 13:44:00 -0800

    added spec file

---
+++ spec/file_sorter_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../models/concerns/file_sorter'
+
+describe FileSorter do
+  describe "#split_file" do
+    let (:file_sorter) { FileSorter.new('source.csv') }
+
+    it "splits a file" do
+      destination = 'chunks'
+      file_sorter.mkdir(destination)
+      file_sorter.split(destination, 10)
+
+      num_of_files = `ls #{destination}`.count("\n")
+      num_of_chunks = file_sorter.file.count.divmod(10)
+      num_of_chunks = if num_of_chunks[1].zero?
+        num_of_chunks[0]
+      else
+        num_of_chunks[0] + 1
+      end
+
+      expect(num_of_files).to eq num_of_chunks
+      file_sorter.rm_dir(destination)
+    end
+  end
+end
