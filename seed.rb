17f71c1273a7b14414f2451c536cf5e1b3496802
Edmund Li <edmund.xz.lee@gmail.com>
2015-12-19 18:41:53 -0800

    use data from db to generate csv file

--- seed.rb
+++ seed.rb
@@ -1,18 +1,41 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require_all 'models', 'controllers'
+
+Neo4j::Session.open(:server_db)
+Neo4j::Transaction.run do
+  100.times do
+    begin
+      User.create(username: Faker::Internet.user_name)
+      print "."
+    rescue Neo4j::Server::CypherResponse::ConstraintViolationError
+      retry
+    end
+  end
+end
+
+puts "\n"
+
 # generate csv file
+
 File.open('source.csv', 'w') do |file|
   content = []
-  user_ids = (1..200).to_a.shuffle
+  user_ids = User.all.map(&:uuid).shuffle
 
-  100.times do
-    user_a_id, user_b_id = user_ids.pop(2)
-    content << "#{user_a_id},#{user_b_id}"
-
-    odd = rand(2)
-
-    content << "#{user_b_id},#{user_a_id}" if odd == 1
+  user_ids.each_with_index do |uid_a, i|
+    user_ids.each_with_index do |uid_b, j|
+      next if i == j
+      odd = rand(2)
+      content << "#{uid_a},#{uid_b}" if odd == 1
+    end
   end
 
   content = content.shuffle.join("\n")
 
   file.write(content)
 end
+
+puts "Database seeded"
