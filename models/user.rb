926ba424794c4e751fe6e98a38da6c2cfa29d738
Edmund Li <edmund.xz.lee@gmail.com>
2015-12-19 18:43:46 -0800

    move user to models dir

---
+++ models/user.rb
@@ -0,0 +1,10 @@
+require 'neo4j'
+
+class User
+  include Neo4j::ActiveNode
+
+  property :username, type: String, constraint: :unique
+
+  has_many :out, :followers, type: :FOLLOWING,   model_class: :User
+  has_many :in,  :followees, origin: :followers, model_class: :User
+end
