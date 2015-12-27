f2997cd50b17483fd5821ad0931b055478894d00
Edmund Li <edmund.xz.lee@gmail.com>
2015-12-27 12:32:11 -0800

    use single thread to write db

--- workers/build_relation_task_worker.rb
+++ workers/build_relation_task_worker.rb
@@ -8,17 +8,58 @@ module Workers
 
     def perform(file_name)
       file = File.open(file_name)
-      error_log_file_name = "error.csv"
 
       batch = []
 
       file.each_with_index do |line, i|
         batch << line
         if i % 1000 == 0
-          BuildRelationWorker.perform_async(batch, error_log_file_name)
+          # BuildRelationWorker.perform_async(batch, error_log_file_name)
+          build_rel(batch)
           batch = []
         end
       end
+    end
+
+    private
+
+    def build_rel(batch)
+      error_log_file_name = "error.csv"
+      Neo4j::Session.open(:server_db)
+
+      error_rows = []
+      Neo4j::Transaction.run do
+        batch.each do |row|
+          # check if matches neo4j uuid pattern
+          unless is_valid_data?(row)
+            error_rows << "#{row.chomp}<=error_msg=>InvalidId"
+            next
+          end
+
+          uid_a, uid_b = row.chomp.split(",")
+
+          begin
+            user_a = User.find(uid_a)
+            user_b = User.find(uid_b)
+          rescue Neo4j::RecordNotFound
+            error_rows << "#{row.chomp}<=error_msg=>RecordNotFound"
+            next
+          end
+
+          if user_a.followers.where(uuid: user_b.uuid).empty?
+            user_a.followers << user_b
+          else
+            error_rows << "#{row.chomp}<=error_msg=>DuplicatedEntry"
+          end
+        end
+        LogErrorWorker.perform_async(error_log_file_name, error_rows)
+        error_rows = []
+      end
+    end
+
+    def is_valid_data?(data)
+      regex = /^[a-z|\d]{8}-([a-z|\d]{4}-){3}[a-z|\d]{12},[a-z|\d]{8}-([a-z|\d]{4}-){3}[a-z|\d]{12}$/i
+      !!(data =~ regex)
     end
   end
 end
