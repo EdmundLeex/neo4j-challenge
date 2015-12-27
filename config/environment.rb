68a259a7a195908fc0dad58b9f12b7eff3fb197d
Edmund Li <edmund.xz.lee@gmail.com>
2015-12-27 11:31:12 -0800

    log sidekiq error

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,17 @@ require 'bundler'
 Bundler.require
 
 require_all 'workers', 'models', 'controllers', 'lib'
+
+puts "Sidekiq error log can be found at '#{Dir.pwd}/sidekiq_error_log.csv'"
+
+Sidekiq.configure_server do |config|
+  config.error_handlers << Proc.new do |ex,ctx_hash|
+    file = CSV.open('sidekiq_error_log.csv', 'a')
+    file << [ex, ctx_hash]
+
+    file.flush
+    file.close
+  end
+end
+
+Neo4j::Session.open(:server_db)
